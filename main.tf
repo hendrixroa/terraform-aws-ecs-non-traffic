fe86826dc0b6db58b4012e3623215aaff9c6b02c
Hendrix Roa <hendrix.alex87@gmail.com>
2020-08-15 16:17:20 -0500

    Fix count resources

--- main.tf
+++ main.tf
@@ -72,14 +72,14 @@ resource "aws_lambda_permission" "main" {
   action        = "lambda:InvokeFunction"
   function_name = var.lambda_stream_arn
   principal     = var.cwl_endpoint
-  source_arn    = "${aws_cloudwatch_log_group.main[count.index].arn}:*"
+  source_arn    = "${aws_cloudwatch_log_group.main.arn}:*"
 }
 
 // Add subscription resource to streaming logs of module to Elasticsearch
 resource "aws_cloudwatch_log_subscription_filter" "main" {
   count           = var.disable_log_streaming ? 0 : 1
   name            = "cloudwatch_${var.name}_logs_to_elasticsearch"
-  log_group_name  = aws_cloudwatch_log_group.main[count.index].name
+  log_group_name  = aws_cloudwatch_log_group.main.name
   filter_pattern  = ""
   destination_arn = var.lambda_stream_arn
   distribution    = "Random"
